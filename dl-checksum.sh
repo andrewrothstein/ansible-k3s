2e0a2b96f6feaa5b237131877eb43864fc6c094f
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-19 14:53:26 -0500

    up to v1.17.0+k3s.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env sh
-VER=${1:-v1.0.0}
 DIR=~/Downloads
-MIRROR=https://github.com/rancher/k3s/releases/download/${VER}
+MIRROR=https://github.com/rancher/k3s/releases/download
 
 dl()
 {
-    local arch=$1
-    url=$MIRROR/sha256sum-${arch}.txt
+    local ver=$1
+    local arch=$2
+    local url=$MIRROR/$ver/sha256sum-${arch}.txt
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $arch `curl -sSL $url | grep k3s | grep -v airgap | awk '{print $1}'`
 }
 
-printf "  %s:\n" $VER
-dl amd64
-dl arm64
-dl arm
+dl_ver() {
+    local ver=$1
+    printf "  %s:\n" $ver
+    dl $ver amd64
+    dl $ver arm64
+    dl $ver arm
+}
 
 
+dl_ver ${1:-v1.17.0+k3s.1}
